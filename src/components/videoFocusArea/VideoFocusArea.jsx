071312286b6141b1d5e07ed4998c45e6fdcf388e
unknown <engr.adnanhossain@gmail.com>
2024-08-15 17:47:59 +0600

    add section

--- src/components/videoFocusArea/VideoFocusArea.jsx
+++ src/components/videoFocusArea/VideoFocusArea.jsx
@@ -1,37 +1,46 @@
+import Image from 'next/image';
 import React from 'react';
 import { FaCircleCheck } from 'react-icons/fa6';
+import icons from '../../../public/assets/icons.png'
 
 const VideoFocusArea = () => {
     return (
         <div className='my-10'>
             <h1 className='text-4xl mb-5 text-center'>Editing Focus Areas</h1>
-            <div className='flex items-center gap-2 mb-2'>
-                <FaCircleCheck className='text-2xl text-green-600' />
-                <p className='text-2xl'>Shorts ( YouTube, Facebook & Tiktok)</p>
-            </div>
-            <div className='flex items-center gap-2 mb-2'>
-                <FaCircleCheck className='text-2xl text-green-600' />
-                <p className='text-2xl'>Promotional videos</p>
-            </div>
-            <div className='flex items-center gap-2 mb-2'>
-                <FaCircleCheck className='text-2xl text-green-600' />
-                <p className='text-2xl'>Travel films & Documentary</p>
-            </div>
-            <div className='flex items-center gap-2 mb-2'>
-                <FaCircleCheck className='text-2xl text-green-600' />
-                <p className='text-2xl'>Motion graphics & Infographic content</p>
-            </div>
-            <div className='flex items-center gap-2 mb-2'>
-                <FaCircleCheck className='text-2xl text-green-600' />
-                <p className='text-2xl'>YouTube & others social media content</p>
-            </div>
-            <div className='flex items-center gap-2 mb-2'>
-                <FaCircleCheck className='text-2xl text-green-600' />
-                <p className='text-2xl'>Storytelling & Storyhead</p>
-            </div>
-            <div className='flex items-center gap-2 mb-2'>
-                <FaCircleCheck className='text-2xl text-green-600' />
-                <p className='text-2xl'>And more</p>
+            <div className='flex items-start justify-center'>
+                <div className='w-1/2'>
+                    <div className='flex items-center gap-2 mb-2'>
+                        <FaCircleCheck className='text-2xl text-green-600' />
+                        <p className='text-2xl'>Shorts ( YouTube, Facebook & Tiktok)</p>
+                    </div>
+                    <div className='flex items-center gap-2 mb-2'>
+                        <FaCircleCheck className='text-2xl text-green-600' />
+                        <p className='text-2xl'>Promotional videos</p>
+                    </div>
+                    <div className='flex items-center gap-2 mb-2'>
+                        <FaCircleCheck className='text-2xl text-green-600' />
+                        <p className='text-2xl'>Travel films & Documentary</p>
+                    </div>
+                    <div className='flex items-center gap-2 mb-2'>
+                        <FaCircleCheck className='text-2xl text-green-600' />
+                        <p className='text-2xl'>Motion graphics & Infographic content</p>
+                    </div>
+                    <div className='flex items-center gap-2 mb-2'>
+                        <FaCircleCheck className='text-2xl text-green-600' />
+                        <p className='text-2xl'>YouTube & others social media content</p>
+                    </div>
+                    <div className='flex items-center gap-2 mb-2'>
+                        <FaCircleCheck className='text-2xl text-green-600' />
+                        <p className='text-2xl'>Storytelling & Storyhead</p>
+                    </div>
+                    <div className='flex items-center gap-2 mb-2'>
+                        <FaCircleCheck className='text-2xl text-green-600' />
+                        <p className='text-2xl'>And more</p>
+                    </div>
+                </div>
+                <div className='w-1/2'>
+                    <Image src={icons} alt='icons' placeholder='blur' quality={100} />
+                </div>
             </div>
         </div>
     );
